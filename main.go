030556889d13e56384fc6abe4abcc8eb265c9eda
Julian Holfeld <julian@uni-kassel.de>
2023-01-13 18:32:10 +0100

    refactor: use fuzzy-search

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-const baseURL = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name="
+const baseURL = "https://db.ygoprodeck.com/api/v7/cardinfo.php?fname="
 
 var (
 	helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
